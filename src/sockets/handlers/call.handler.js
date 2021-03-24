e93ee37e45b075e394ad1635abfb6cb9dbbdbc53
huynhthaianhhd <huynhthaianhhd@gmail.com>
2021-03-24 19:44:59 +0700

    GEN-199: implement socket callVideo

---
+++ src/sockets/handlers/call.handler.js
@@ -0,0 +1,34 @@
+import { onlineUsers } from '../models';
+import { messageService } from 'services';
+
+const callHandler = (io, socket) => {
+  socket.on('call:callVideo', async ({ userId }) => {
+    console.log({ userId });
+    const socketIds = await onlineUsers.getSocketIdsByUserId(userId);
+    const userCall = await onlineUsers.getUserBySocketId(socket.id);
+    console.log('socket.id', onlineUsers, socket.id);
+    console.log('userCall', userCall);
+    socketIds.forEach((socketId) =>
+      io.to(socketId).emit('call:notifyCall', {
+        userCall,
+      }),
+    );
+  });
+  socket.on('call:acceptCall', async ({ userId }) => {
+    const userBeCalled = await onlineUsers.getUserBySocketId(socket.id);
+    const userCall = await onlineUsers.getUserById(userId);
+    const socketIds = [
+      ...(await onlineUsers.getSocketIdsByUserId(userBeCalled.id)),
+      ...(await onlineUsers.getSocketIdsByUserId(userId)),
+    ];
+    console.log('socketIds', socketIds);
+    socketIds.forEach((socketId) =>
+      io.to(socketId).emit('call:acceptedCall', {
+        userCall,
+        userBeCalled,
+      }),
+    );
+  });
+};
+
+export default callHandler;
