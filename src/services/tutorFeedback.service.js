c1a07d41a99e3e4cfcdc1d82f8281788661532ac
huynhthaianhhd <55442707+huynhthaianhhd@users.noreply.github.com>
2021-04-21 22:08:01 +0700

    NOJIRA : Fix feedback tutor (#81)
    * NOJIRA: fix feedback tutor

--- src/services/tutorFeedback.service.js
+++ src/services/tutorFeedback.service.js
@@ -1,3 +1,4 @@
+import e from 'cors';
 import { TutorFeedback } from 'database/models';
 const feedbackService = {};
 
@@ -7,11 +8,31 @@ feedbackService.feedbackTutor = async ({
   content,
   rating,
 }) => {
-  return await TutorFeedback.create({
-    firstId,
-    secondId,
-    content,
-    rating,
+  const feedback = await TutorFeedback.findOne({
+    where: {
+      firstId,
+      secondId,
+    },
   });
+  if (feedback) {
+    const fields = {
+      content,
+      rating,
+    };
+    return await TutorFeedback.update(fields, {
+      where: {
+        firstId,
+        secondId,
+      },
+    });
+  } else {
+    return await TutorFeedback.create({
+      firstId,
+      secondId,
+      content,
+      rating,
+    });
+  }
 };
+
 export default feedbackService;
