163426efb18e603daa28626c08200f4a83e69ca3
huynhthaianhhd <huynhthaianhhd@gmail.com>
2021-05-08 12:11:45 +0700

    NOJIRA: handle call video

--- src/sockets/handlers/call.handler.js
+++ src/sockets/handlers/call.handler.js
@@ -1,4 +1,6 @@
 import { onlineUsers } from '../controllers';
+import jwt from 'jsonwebtoken';
+import { jwt as jwtVar } from 'configs/vars';
 
 const callHandler = (io, socket) => {
   socket.on('call:callVideo', async ({ userId }) => {
@@ -11,14 +13,33 @@ const callHandler = (io, socket) => {
     );
   });
   socket.on('call:acceptCall', async ({ userId, startTime }) => {
-    const userBeCalled = await onlineUsers.getUserBySocketId(socket.id);
+    const userAccepted = await onlineUsers.getUserBySocketId(socket.id);
     const userCall = await onlineUsers.getUserById(userId);
-    const socketIds = [...(await onlineUsers.getSocketIdsByUserId(userId))];
+    const socketIds = [
+      ...(await onlineUsers.getSocketIdsByUserId(userId)),
+      ...(await onlineUsers.getSocketIdsByUserId(userAccepted.id)),
+    ];
+    const jwtRoom = jwt.sign({}, jwtVar.secret);
+    const roomName = userAccepted.name + '-' + userCall.name;
     socketIds.forEach((socketId) =>
       io.to(socketId).emit('call:acceptedCall', {
         userCall,
-        userBeCalled,
+        userBeCalled: userAccepted,
+        jwtRoom,
+        roomName,
         startTime,
+      }),
+    );
+  });
+
+  socket.on('call:cancelCall', async ({ userId }) => {
+    const userCancel = await onlineUsers.getUserBySocketId(socket.id);
+    const userBeCalled = await onlineUsers.getUserById(userId);
+    const socketIds = [...(await onlineUsers.getSocketIdsByUserId(userId))];
+    socketIds.forEach((socketId) =>
+      io.to(socketId).emit('call:canceledCall', {
+        userCall: userCancel,
+        userBeCalled,
       }),
     );
   });
