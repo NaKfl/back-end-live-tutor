3b9b071e6570f15bd47e21c1f59dc0a718a8e81b
Nguyen Huu Gia Tri <44458003+NaKfl@users.noreply.github.com>
2021-05-25 23:37:50 +0700

    Add count (#114)

--- src/apidoc/payment.doc.js
+++ src/apidoc/payment.doc.js
@@ -74,43 +74,45 @@
  * @apiSuccessExample Success Response Example:
 {
     "message": "Get transaction's history successfully",
-    "data": [
-        {
-            "id": "1cb16cdc-0497-42f6-8f81-43ce4714475f",
-            "walletId": "12e8afec-887c-418e-b419-d997b8e9cf6f",
-            "bookingId": "3a27402f-7e41-4563-9485-17fe13926382",
-            "price": "-100000",
-            "status": "success",
-            "createdAt": "2021-05-25T12:34:22.793Z",
-            "updatedAt": "2021-05-25T12:34:22.793Z",
-            "bookingInfo": {
-                "id": "3a27402f-7e41-4563-9485-17fe13926382",
-                "userId": "18e8afec-887c-418e-b419-d997b8e9cf6f",
-                "scheduleDetailId": "5ebb503b-f038-4dd4-8d61-74cc698ac04d",
-                "createdAt": "2021-05-25T12:34:22.759Z",
-                "updatedAt": "2021-05-25T12:34:22.759Z"
+    "data": {
+        "count": 2,
+        "rows": [
+            {
+                "id": "3f6d9f5c-e2c1-40f2-9105-dc26b4f14512",
+                "walletId": "12e8afec-887c-418e-b419-d997b8e9cf6f",
+                "bookingId": "4f1a997e-3175-4da3-beb4-00b690e3efba",
+                "price": "-100000",
+                "status": "success",
+                "createdAt": "2021-05-25T16:01:35.309Z",
+                "updatedAt": "2021-05-25T16:01:35.309Z",
+                "bookingInfo": {
+                    "id": "4f1a997e-3175-4da3-beb4-00b690e3efba",
+                    "scheduleDetailId": "62fc9562-371a-4695-8c67-0da181dbc3c5",
+                    "createdAt": "2021-05-25T16:01:35.274Z",
+                    "scheduleDetailInfo": {
+                        "id": "62fc9562-371a-4695-8c67-0da181dbc3c5",
+                        "startPeriod": "03:30",
+                        "endPeriod": "04:00",
+                        "tutorInfo": {
+                            "id": "11deb760-de73-46cb-b636-64f00da6f7d8",
+                            "name": "Tri Nguyen",
+                            "email": "nguyenhuugiatri@gmail.com",
+                            "avatar": "http://localhost:3333/avatar/11deb760-de73-46cb-b636-64f00da6f7d8avatar1621946320677.png"
+                        }
+                    }
+                }
+            },
+            {
+                "id": "99f9ba16-180f-48e1-a708-67449e0c076a",
+                "walletId": "12e8afec-887c-418e-b419-d997b8e9cf6f",
+                "bookingId": null,
+                "price": "1000000",
+                "status": "success",
+                "createdAt": "2021-05-25T12:28:08.142Z",
+                "updatedAt": "2021-05-25T12:28:08.142Z",
+                "bookingInfo": null
             }
-        },
-        {
-            "id": "f5416ab8-8103-4a3a-855d-89ab4041b6d8",
-            "walletId": "12e8afec-887c-418e-b419-d997b8e9cf6f",
-            "bookingId": null,
-            "price": "40000",
-            "status": "success",
-            "createdAt": "2021-05-25T12:34:22.793Z",
-            "updatedAt": "2021-05-25T12:34:22.793Z",
-            "bookingInfo": null
-        },
-        {
-            "id": "b1859165-c25b-4f82-a646-6065229782a7",
-            "walletId": "12e8afec-887c-418e-b419-d997b8e9cf6f",
-            "bookingId": null,
-            "price": "1000",
-            "status": "success",
-            "createdAt": "2021-05-25T12:34:22.793Z",
-            "updatedAt": "2021-05-25T12:34:22.793Z",
-            "bookingInfo": null
-        }
-    ]
+        ]
+    }
 }
  */
