000ec560321c3033317c7765583d971ef88aa622
NaKfl <nguyenhuugiatri@gmail.com>
2021-06-23 07:11:19 +0700

    Cluster mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: './build/index.js',
       watch: true,
       ignore_watch: ['node_modules', 'build'],
+      instances: 4,
       env: {
         NODE_ENV: 'development',
       },
